e6b643cfe0528fd1ac4ec422a789aae9e579708b
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-03-02 13:30:03 +0200

    Add comments

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,14 +1,43 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+/**
+ * @notice A simple voting contract
+ */
 contract Voting {
+    /**
+     * @notice Which addresses are voters
+     */
     mapping(address => bool) public isVoter;
-    mapping(address => bool) public hasVoted;
-    uint256 public yesVotes = 0;
-    uint256 public noVotes = 0;
-    uint256 public voterCount = 0;
-    address target;
 
+    /**
+     * @notice Which addresses have already voted
+     */
+    mapping(address => bool) public hasVoted;
+
+    /**
+     * @notice How many 'yes' votes
+     */
+    uint256 public yesVotes = 0;
+
+    /**
+     * @notice How many 'no' votes
+     */
+    uint256 public noVotes = 0;
+
+    /**
+     * @notice How many voters exist in total
+     */
+    uint256 public voterCount = 0;
+
+    /**
+     * @notice Current suggested charity target address
+     */
+    address public target;
+
+    /**
+     * @notice Instantiates the contract
+     */
     constructor(address[] memory voters) {
         for (uint256 i = 0; i < voters.length; i++) {
             isVoter[voters[i]] = true;
@@ -16,6 +45,9 @@ contract Voting {
         }
     }
 
+    /**
+     * @notice Any voter can suggest a new target for the charity assets. Works only if no target set yet.
+     */
     function suggestTarget(address suggestedTarget) public {
         require(isVoter[msg.sender] == true, "Not a voter");
         if (target == address(0x0)) {
@@ -23,6 +55,9 @@ contract Voting {
         }
     }
 
+    /**
+     * @notice Vote on a suggested charity target
+     */
     function vote(bool userVote) public {
         require(isVoter[msg.sender] == true, "Not a voter");
         require(!hasVoted[msg.sender], "Already voted");
@@ -35,6 +70,9 @@ contract Voting {
         }
     }
 
+    /**
+     * @notice Distributes the assets to the chosen charity address if the vote passes
+     */
     function distribute() public {
         require(yesVotes + noVotes >= voterCount, "Not enough votes");
         if (yesVotes > noVotes) {
@@ -43,5 +81,8 @@ contract Voting {
         // TODO: reset the contract state so a new vote can be initiated
     }
 
+    /**
+     * @notice Deposit native assets to the contract
+     */
     function deposit() public payable {}
 }
