7a8b81e81cbb077df38650a08aad4391e519d469
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-03-03 09:11:43 +0200

    add Sepolia deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+import "@nomiclabs/hardhat-etherscan";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_PROVIDER_URL,
+      accounts: ['0x' + process.env.SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,42 @@
 import { ethers } from "hardhat";
+import hre from "hardhat";
 
 // This is an example deployment script. 
 // You can use this to deploy to any network, but the project is not configured to access any real networks currently.
 
+const verify = async (addr: string, args: any[]) => {
+  try {
+    await hre.run("verify:verify", {
+      address: addr,
+      constructorArguments: args,
+    });
+  } catch (ex: any) {
+    if (ex.toString().indexOf("Already Verified") == -1) {
+      throw ex;
+    }
+  }
+};
+
 async function main() {
   const signers = await ethers.getSigners();
 
-  const voter1 = signers[0];
-  const voter2 = signers[1];
-  const voter3 = signers[2];
+  const voter1 = signers[0].address; // Address based on the deployer's private key
+  const voter2 = "0xd0F723c6b2226dF56Fe41E63b9eAA66Eb540BcB8"; // Alice's public address
+  const voter3 = "0x0d731cfabC5574329823F26d488416451d2ea376"; // Bob's public address
 
-  let voters = [voter1.address, voter2.address, voter3.address];
+  let voters = [voter1, voter2, voter3];
 
   const VotingFactory = await ethers.getContractFactory("Voting");
   const votingContract = await VotingFactory.connect(signers[0]).deploy(voters);
 
   await votingContract.deployed();
 
-  console.log(`Contract deployed to ${votingContract.address}`);
+  console.log("Deployments done, waiting for etherscan verifications");
+  // Wait for the contract to be propagated inside Etherscan
+  await new Promise((f) => setTimeout(f, 60000));
+  await verify(votingContract.address, [voters]);
+
+  console.log(`Contract deployed and verified at https://sepolia.etherscan.io/address/${votingContract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
